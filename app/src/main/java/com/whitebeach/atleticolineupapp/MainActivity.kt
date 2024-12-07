7149cbbf3b90bdc080f2b27cbc639a28d15a74db
write18jack <write18jack@gmail.com>
2024-12-07 13:02:45 +0900

    edit app module files

--- app/src/main/java/com/whitebeach/atleticolineupapp/MainActivity.kt
+++ app/src/main/java/com/whitebeach/atleticolineupapp/MainActivity.kt
@@ -1,17 +1,13 @@
-package com.whitebeach.atleticolineupapp.presentations.main
+package com.whitebeach.atleticolineupapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.whitebeach.presentation.ui.dragDrop.DragContainer
 import com.whitebeach.atleticolineupapp.app.theme.AtleticoLineupAppTheme
-import com.whitebeach.atleticolineupapp.app.component.dragDrop.DragContainer
+import com.whitebeach.presentation.ui.main.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
