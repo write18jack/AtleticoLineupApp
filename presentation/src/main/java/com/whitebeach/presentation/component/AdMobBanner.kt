c94107cd89bcb22dbcfb516a9e816ea5aa75f9ad
write18jack <write18jack@gmail.com>
2025-06-03 19:55:52 +0900

    adMobのIDコメントアウト

--- presentation/src/main/java/com/whitebeach/presentation/component/AdMobBanner.kt
+++ presentation/src/main/java/com/whitebeach/presentation/component/AdMobBanner.kt
@@ -7,11 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
-import com.whitebeach.presentation.R
 
 @Composable
 fun AdaptiveBanner(
@@ -24,21 +19,21 @@ fun AdaptiveBanner(
             .padding(8.dp)
             .fillMaxWidth(),
     ) {
-        AndroidView(
-            modifier = Modifier.fillMaxWidth(),
-            factory = {context ->
-                // Using application context to avoid memory leak
-                AdView(context).apply {
-                    setAdSize(
-                        AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
-                            context,
-                            deviceCurrentWidth,
-                        ),
-                    )
-                    adUnitId = context.getString(R.string.ad_id_banner)
-                    loadAd(AdRequest.Builder().build())
-                }
-            },
-        )
+//        AndroidView(
+//            modifier = Modifier.fillMaxWidth(),
+//            factory = {context ->
+//                // Using application context to avoid memory leak
+//                AdView(context).apply {
+//                    setAdSize(
+//                        AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+//                            context,
+//                            deviceCurrentWidth,
+//                        ),
+//                    )
+//                    adUnitId = context.getString(R.string.ad_id_banner)
+//                    loadAd(AdRequest.Builder().build())
+//                }
+//            },
+//        )
     }
 }
